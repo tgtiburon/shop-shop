02198c56a1ef6ace216c5ac833431208fb2b8394
tgtiburon <tg.tiburon@gmail.com>
2022-02-28 17:44:49 -0500

    initial commit

---
+++ client/src/utils/actions.js
@@ -0,0 +1,6 @@
+// Used by ProductList component
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// take list of categories from server by apollo and store
+// in the global state
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
