ad6ce7da85ceb93744737460d2a02c82af2d2762
tgtiburon <tg.tiburon@gmail.com>
2022-03-04 15:22:35 -0500

    payment goes through

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,11 +4,27 @@ import Auth from "../../utils/auth";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from "@apollo/client";
 
 import "./style.css";
 
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+  // data will contain checkout session but only after
+  // getCheckout() function is called
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
   // check to see if there is anything in the state's cart
   // if not, load data from indexedDB
@@ -47,8 +63,33 @@ const Cart = () => {
     return sum.toFixed(2);
   }
 
+  function submitCheckout() {
+    const productIds = [];
+    // loop over items in cart
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        // this productIds is what QUERY_CHECKOUT will use in the
+        // stripe session
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds },
+    });
+  }
+
+  // useEffect(() => {
+  //   if (data) {
+  //     stripePromise.then((res) => {
+  //       res.redirectToCheckout({ sessionId: data.checkout.session });
+  //     });
+  //   }
+  // }, [data]);
+
   // TODO:
-  console.log(state);
+  // console.log(state);
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>
@@ -63,7 +104,7 @@ const Cart = () => {
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
             {Auth.loggedIn() ? (
-              <button>Checkout</button>
+              <button onClick={submitCheckout}>Checkout</button>
             ) : (
               <span>(log in to check out)</span>
             )}
